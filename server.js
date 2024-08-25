fa04339142fc5908e630022833aed51bf24dee05
Basith <basithmus2021@gmail.com>
2024-08-26 00:14:33 +0530

    Completed

--- server.js
+++ server.js
@@ -22,7 +22,15 @@ const productsRoutes = require('./routes/products-routes');
 app.use('/api/auth', usersRoutes);
 app.use('/api/products', productsRoutes);
 
+app.get('/reload', (req, res) => {
+    res.status(200).json({ message: 'Reloaded!' });
+}); 
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+setInterval(() => {
+    fetch('http://localhost:3000/reload').then(response => response.json()).then(data => console.log(data));
+}, 5000);
